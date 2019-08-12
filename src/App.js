ed3a8598aa7fd6ef6d2d5d676b4b38e09beca6a8
Sara Alkhamri <s.alkhamri@gmail.com>
2019-08-12 13:01:13 -0700

    Changed hardcoded values to dynamic values. Step three completed

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import BottomRow from "./BottomRow";
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
+  
   //declare variables
 
   const [home, setHome] = useState(32);
@@ -19,13 +20,14 @@ function App() {
             <h2 className="home__name">Lions</h2>
 
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
-
-            <div className="home__score">{home}</div>
+            {/* change to dynamic value */}
+            <div className="home__score">{home}</div> 
           </div>
           <div className="timer">00:03</div>
           <div className="away">
             <h2 className="away__name">Tigers</h2>
-            <div className="away__score">{away}</div>
+            {/* change to dynamic value */}
+            <div className="away__score">{away}</div> 
           </div>
         </div>
         <BottomRow />
